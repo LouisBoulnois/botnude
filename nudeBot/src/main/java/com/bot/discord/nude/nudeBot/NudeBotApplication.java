880e2ec4c8f3900e49dc75ce7b412044bc2b595d
LouisBoulnois <boulnois.louis@gmail.com>
2019-03-04 00:52:34 +0100

    init projet et ajout corp API

---
+++ nudeBot/src/main/java/com/bot/discord/nude/nudeBot/NudeBotApplication.java
@@ -0,0 +1,13 @@
+package com.bot.discord.nude.nudeBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NudeBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NudeBotApplication.class, args);
+	}
+
+}
---
+++ nudeBot/src/test/java/com/bot/discord/nude/nudeBot/NudeBotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bot.discord.nude.nudeBot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NudeBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
